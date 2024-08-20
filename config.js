57f3754e62ecc5fac20290ca2a9c752601d747e3
Eboxsl <Eboxsl@hotmail.com>
2024-08-20 13:27:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "UVIgQIyQ#hnBmpp1VptZRsDjKJD5qojbsbHPZAVXjRK4NH-lP1U4",
-ALIVE_IMG: process.env.ALIVE_IMG || "",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/b53a8448a280837695c3c.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "Elix Is Alivwme👍🇱🇰✅",
 };
