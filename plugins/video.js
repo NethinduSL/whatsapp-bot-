d8c8960944163406680ca9dd36b4ce9230158b7c
Eboxsl <Eboxsl@hotmail.com>
2024-08-21 11:23:04 +0530

    Update video.js

--- plugins/video.js
+++ plugins/video.js
@@ -1,51 +1,45 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const fg = require('api-dylux');
+const yts = require('yt-search');
 
-const config = require('../config')
-const {cmd , commands} = require('../command')
-const fg = requir('api-dylux')
-const yts = requir('yt-search')
 cmd({
     pattern: "song",
     desc: "Download Songs By Elixa.",
     category: "Download",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if (!q) return reply("Please give me a valid URL 🙃")
-const search = await yts(q)
-const data= search.videos[0];
-const url= data.url
-    
-let desc =`
+async (conn, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+    try {
+        if (!q) return reply("Please provide a valid URL 🙃");
+        const search = await yts(q);
+        const data = search.videos[0];
+
+        let desc = `
 *Elixa Song Downloader*🎵
 
-title: ${data.title}
-description: ${data.description}
-time: ${data.timestamp}
-ago : ${data.ago}
-views: ${dat.views}
+Title: ${data.title}
+Description: ${data.description}
+Duration: ${data.timestamp}
+Uploaded: ${data.ago}
+Views: ${data.views}
 
 > ®𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗
+`;
 
-`
-await conn.sendMessage(from,{image:{url:data.thumbnail},caption:desc},{quoted:mek});
+        await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
+        // download audio
+        let down = await fg.yta(data.url); // Use the video's URL for downloading
+        let downloadUrl = down.dl_link;
 
-// download audio
-let down = await fg.yta()
-let downloadUrl = down.dl_url
+        // send audio
+        await conn.sendMessage(from, { audio: { url: downloadUrl }, mimetype: "audio/mpeg" }, { quoted: mek });
 
-//send audio
-
-
-await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg"},{quated:mek});   
-
-
-  
-}catch(e){
-comnsole.log(e)
-reply(`${e}`)
-  
-}
-
-})
+    } catch (e) {
+        console.log(e);
+        reply(`Error: ${e}`);
+    }
+});
