d46e7ebbafe1ffe3b30242b9a1cc4f0712756456
Eboxsl <Eboxsl@hotmail.com>
2024-08-22 15:45:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "UVIgQIyQ#hnBmpp1VptZRsDjKJD5qojbsbHPZAVXjRK4NH-lP1U4",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/b53a8448a280837695c3c.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "Elix Is Alive NoW👍🇱🇰✅",
+AUTO_READ_STATUS:process.env.AUTO_READ_STATUS || "true",
 };
