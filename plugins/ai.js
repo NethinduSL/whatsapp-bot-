9b0c98443a488d94994ef14f2f064bfa7d6c47bd
Eboxsl <Eboxsl@hotmail.com>
2024-08-22 15:52:41 +0530

    Create ai.js

---
+++ plugins/ai.js
@@ -0,0 +1,21 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+cmd({
+    pattern: "ai",
+    desc: "Check bot online or no.",
+    category: "main",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+
+
+
+        
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
