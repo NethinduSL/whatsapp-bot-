9fe1de0bc2230a23d3e1232d1b203286fa5142b2
Eboxsl <Eboxsl@hotmail.com>
2024-08-22 14:20:19 +0530

    Update Downloader.js

--- plugins/Downloader.js
+++ plugins/Downloader.js
@@ -16,7 +16,7 @@ async (conn, mek, m, {
         if (!q) return reply("Please provide a valid URL 🙃");
         const search = await yts(q);
         const data = search.videos[0];
-        const url = data.url
+        const url = data.url;
 
         let desc = `
 ╭𝗘ꟾ𝖎✘𝗮 𝗠𝗗 𝗦𝗼𝗻𝗴 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿🎵╮
@@ -25,11 +25,10 @@ async (conn, mek, m, {
 ╰🔗𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${data.description}
 ╰🕦𝗧𝗶𝗺𝗲: ${data.timestamp}
 ╰📤𝗔𝗴𝗼: ${data.ago}
-╰👁️𝗩𝗶𝗲𝘄𝘀: ${dat.views}
+╰👁️𝗩𝗶𝗲𝘄𝘀: ${data.views}
 
 
 > 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺
-
 `;
 
         await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
@@ -39,24 +38,16 @@ async (conn, mek, m, {
         let downloadUrl = down.dl_url;
 
         // send audio
-        await conn.sendMessage(from,  {audio:{url: downloadUrl}, mimetype: "audio/mpeg" }, { quoted: mek });
-       await conn.sendMessage(from,  {document:{url: downloadUrl}, mimetype: "audio/mp3",fileName:data.title + ".mp3", caption:"®𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗"}, { quoted: mek });
-       
- 
+        await conn.sendMessage(from, { audio: { url: downloadUrl }, mimetype: "audio/mpeg" }, { quoted: mek });
+        await conn.sendMessage(from, { document: { url: downloadUrl }, mimetype: "audio/mp3", fileName: data.title + ".mp3", caption: "®𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗" }, { quoted: mek });
 
-
-        
     } catch (e) {
         console.log(e);
         reply(`Error: ${e}`);
     }
 });
 
-
-
-
 //---------------------------video--------------------------
-
 
 cmd({
     pattern: "video",
@@ -71,7 +62,7 @@ async (conn, mek, m, {
         if (!q) return reply("Please provide a valid URL 🙃");
         const search = await yts(q);
         const data = search.videos[0];
-        const url = data.url
+        const url = data.url;
 
         let desc = `        
 ╭𝗘ꟾ𝖎✘𝗮 𝗠𝗗 𝗩𝗶𝗱𝗲𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿🎬╮
@@ -80,7 +71,7 @@ async (conn, mek, m, {
 ╰🔗𝗗𝗲𝘀𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: ${data.description}
 ╰🕦𝗧𝗶𝗺𝗲: ${data.timestamp}
 ╰📤𝗔𝗴𝗼: ${data.ago}
-╰👁️𝗩𝗶𝗲𝘄𝘀: ${dat.views}
+╰👁️𝗩𝗶𝗲𝘄𝘀: ${data.views}
 
 
 > 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺
@@ -88,21 +79,16 @@ async (conn, mek, m, {
 
         await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
-        // download audio
-        let down = await fg.yta(url); // Use the video's URL for downloading
+        // download video
+        let down = await fg.ytv(url); // Use the video's URL for downloading
         let downloadUrl = down.dl_url;
 
         // send video
-       await conn.sendMessage(from,  {video:{url: downloadUrl}, mimetype: "video/mp4" }, { quoted: mek });
-       await conn.sendMessage(from,  {document:{url: downloadUrl}, mimetype: "video/mp4",fileName:data.title + ".mp4",caption:"®𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗 }, { quoted: mek });
-       
- 
+        await conn.sendMessage(from, { video: { url: downloadUrl }, mimetype: "video/mp4" }, { quoted: mek });
+        await conn.sendMessage(from, { document: { url: downloadUrl }, mimetype: "video/mp4", fileName: data.title + ".mp4", caption: "®𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗" }, { quoted: mek });
 
     } catch (e) {
         console.log(e);
         reply(`Error: ${e}`);
     }
 });
-
-
-
