f6ccbb92be9a99250f957ecd8240aed8798b3bed
Eboxsl <Eboxsl@hotmail.com>
2024-08-22 14:06:25 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,5 +1,5 @@
 const config = require('../config')
-const {cmd , commands} = require('../command')
+const { cmd, commands } = require('../command')
 
 cmd({
     pattern: "alive",
@@ -7,11 +7,14 @@ cmd({
     category: "main",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG,caption:"> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺"},{quoted: mek})
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        return await conn.sendMessage(from, {
+            image: { url: config.ALIVE_IMG },
+            caption: `${config.ALIVE_MSG}\n> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺`
+        }, { quoted: mek })
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
 })
