5156880faa8d518daeff5cd8eb11e484c78f3092
Eboxsl <Eboxsl@hotmail.com>
2024-08-23 14:09:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID || "UVIgQIyQ#hnBmpp1VptZRsDjKJD5qojbsbHPZAVXj
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/b53a8448a280837695c3c.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "Elix Is Alive NoW👍🇱🇰✅",
 AUTO_READ_STATUS:process.env.AUTO_READ_STATUS || "true",
+OWNER :process.env.OWNER || "Your Name"
 };
