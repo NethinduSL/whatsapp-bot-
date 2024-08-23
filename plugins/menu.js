5a8590daa49def3f0e19f534e7641da6ec450c47
Eboxsl <Eboxsl@hotmail.com>
2024-08-23 13:27:21 +0530

    Create menu.js

---
+++ plugins/menu.js
@@ -0,0 +1,76 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "menu",
+    desc: "Menu of Elixa",
+    category: "main",
+    filename: __filename
+}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        let menu = {
+            main: '',
+            download: '',
+            group: '',
+            games: '',
+            ai: '',
+            admin: '',
+            search: '',
+            converter: ''
+        };
+
+        for (let i = 0; i < commands.length; i++) {
+            if (commands[i].pattern && !commands[i].dontAddCommandList) {
+                menu[commands[i].category] += `.${commands[i].pattern}\n│`;
+            }
+        }
+
+        let Elixamenu = `
+╭𝗛𝗲𝗹𝗹𝗼 ${pushname} 𝗘ꟾ𝖎✘𝗮👋╮
+│
+│𝗶𝗻 𝗚𝗿𝗼𝘂𝗽 ${groupName}
+│𝗣𝗮𝗿𝘁𝗶𝗰𝗶𝗽𝗮𝗻𝘁𝘀 ${participants}
+│𝗬𝗼𝘂𝗿 𝗡𝘂𝗺𝗯𝗲𝗿 ${senderNumber}
+│𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺
+│
+╰
+╰𝗠𝗮𝗶𝗻 𝗠𝗘𝗡𝗨 ✅
+
+${menu.main}
+
+╰𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿 𝗠𝗘𝗡𝗨 ⬇️
+
+${menu.download}
+
+╰𝗚𝗿𝗼𝘂𝗽 𝗠𝗘𝗡𝗨 👥
+
+${menu.group}
+
+╰𝗔𝗶 𝗠𝗘𝗡𝗨 🦾
+
+${menu.ai}
+
+╰𝗔𝗱𝗺𝗶𝗻 𝗠𝗘𝗡𝗨 🧑‍💻
+
+${menu.admin}
+
+╰𝗦𝗲𝗮𝗿𝗰𝗵 𝗠𝗘𝗡𝗨 🔎
+
+${menu.search}
+
+╰𝗖𝗼𝗻𝘃𝗲𝗿𝘁𝗲𝗿 𝗠𝗘𝗡𝗨 🧪
+
+${menu.converter}
+
+> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺
+        `;
+
+
+await conn.sendMessage(from, { text: Elixamenu }, { quoted: mek });
+
+
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
