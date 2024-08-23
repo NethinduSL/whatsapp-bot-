557b77b02a3ab9feea114f3764a3e7c2858e656e
Eboxsl <Eboxsl@hotmail.com>
2024-08-23 15:34:02 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,6 +3,7 @@ const { cmd, commands } = require('../command')
 
 cmd({
     pattern: "alive",
+    alias: ["Nethindu", "elixa", "nethindu"]
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
