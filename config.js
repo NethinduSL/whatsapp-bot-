1981f0aeb182e749376da986c8caf140f2ae5b50
Eboxsl <Eboxsl@hotmail.com>
2024-08-31 21:42:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 module.exports = {
 
-    
+//╭──────────────────────Main──────────────────────╮//   
     SESSION_ID: process.env.SESSION_ID || "UVIgQIyQ#hnBmpp1VptZRsDjKJD5qojbsbHPZAVXjRK4NH-lP1U4",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/261c717e5e4f165712ec4.png",
     ALIVE_MSG: process.env.ALIVE_MSG || "Elix Is Alive Nox👍🇱🇰✅ ",
@@ -12,7 +12,7 @@ module.exports = {
     MODE: process.env.MODE || "public",
     OWNER_NUMBER: process.env.OWNER_NUMBER || "94763936166",
 
-    //╭────────────────Auto────────────────╮//
+//╭──────────────────────Auto──────────────────────╮//
     AUTO_REPLY: process.env.AUTO_REPLY || "true",
     AUTO_STICKER: process.env.AUTO_STICKER || "true",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
