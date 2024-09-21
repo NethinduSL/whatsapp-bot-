06bda25e3d3d2289a5892854ee92f697b2d9e959
Eboxsl <Eboxsl@hotmail.com>
2024-09-21 14:11:04 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -17,11 +17,14 @@ async (conn, mek, m, {
     try {
         const Alive = `
 ${config.ALIVE_MSG}
-𝗥𝘂𝗻𝘁𝗶𝗺𝗲: ${runtime(process.uptime())}
-𝗥𝗮𝗺: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(os.totalmem() / 1024 / 1024)} MB
-𝗛𝗼𝘀𝘁: ${os.hostname()}
-𝗖𝗿𝗲𝗮𝘁𝗼𝗿: Nethindu Thaminda
-𝗢𝘄𝗻𝗲𝗿: ${config.OWNER}
+╭
+│𝗥𝘂𝗻𝘁𝗶𝗺𝗲: ${runtime(process.uptime())}
+│𝗥𝗮𝗺: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(os.totalmem() / 1024 / 1024)} MB
+│𝗛𝗼𝘀𝘁: ${os.hostname()}
+│𝗖𝗿𝗲𝗮𝘁𝗼𝗿: Nethindu Thaminda
+│𝗢𝘄𝗻𝗲𝗿: ${config.OWNER}
+╰───────────────────
+
 > 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 𝗯𝘆 𝗘𝗹𝗶𝘅𝗮 𝗠𝗗`;
         
         return await conn.sendMessage(from, {
