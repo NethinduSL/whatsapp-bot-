3d008e5c6cb1f6f09363069e1f78ece3952bf4ee
Eboxsl <Eboxsl@hotmail.com>
2024-12-02 15:11:38 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,20 +1,14 @@
-const config = require('../config');
-const { cmd, commands } = require('../command');
-const { fetchJson } = require('../lib/functions');
+//මේක බලල වැඩක් නෑ obfuscate කරල තියෙනේ .
 
-cmd({
-    pattern: "ai",
-    desc: "Chat GPT 4.",
-    category: "ai",
-    react :"💫",
-    filename: __filename
-},
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        let data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`);
-        return reply(`${data.data}`);
-    } catch (e) {
-        console.log(e);
-        reply(`${e}`);
-    }
-});
+
+//  ________  __    _                   ____    ____  ______   
+// |_   __  |[  |  (_)                 |_   \  /   _||_   _ `. 
+//   | |_ \_| | |  __   _   __  ,--.     |   \/   |    | | `. \
+//   |  _| _  | | [  | [ \ [  ]` _\ :    | |\  /| |    | |  | |
+//  _| |__/ | | |  | |  >    < // | |,  _| |_\/_| |_  _| |_.  /
+// |________|[___][___][__]`\_]\ -;__/ |_____||_____||______.  
+//All right resevd 🇱🇰
+//❤️ for al users
+
+
+const _0x261a0f=_0x5b9d;function _0x415a(){const _0x48275e=['5219163qGechv','../command','1011963gxLbmz','7934562ShsjhS','../config','Chat\x20GPT\x204.','2286184ZqKHgL','2PJmxhL','5653668vYDRYd','12749884RswaTy','627065hEgRfG','data','../lib/functions','8IWIhyu'];_0x415a=function(){return _0x48275e;};return _0x415a();}(function(_0x3727d0,_0x4686dd){const _0x2d9439=_0x5b9d,_0xf8e101=_0x3727d0();while(!![]){try{const _0x208434=parseInt(_0x2d9439(0xe0))/0x1+-parseInt(_0x2d9439(0xe5))/0x2*(parseInt(_0x2d9439(0xe6))/0x3)+-parseInt(_0x2d9439(0xe4))/0x4+-parseInt(_0x2d9439(0xe8))/0x5+parseInt(_0x2d9439(0xe1))/0x6+parseInt(_0x2d9439(0xe7))/0x7*(parseInt(_0x2d9439(0xdd))/0x8)+-parseInt(_0x2d9439(0xde))/0x9;if(_0x208434===_0x4686dd)break;else _0xf8e101['push'](_0xf8e101['shift']());}catch(_0x5b0859){_0xf8e101['push'](_0xf8e101['shift']());}}}(_0x415a,0xf2c4c));function _0x5b9d(_0x5dca34,_0x3d6722){const _0x415a9b=_0x415a();return _0x5b9d=function(_0x5b9dd2,_0x15f4c6){_0x5b9dd2=_0x5b9dd2-0xdb;let _0xc256fb=_0x415a9b[_0x5b9dd2];return _0xc256fb;},_0x5b9d(_0x5dca34,_0x3d6722);}const config=require(_0x261a0f(0xe2)),{cmd,commands}=require(_0x261a0f(0xdf)),{fetchJson}=require(_0x261a0f(0xdc));cmd({'pattern':'ai','desc':_0x261a0f(0xe3),'category':'ai','react':'💫','filename':__filename},async(_0x1effa4,_0x282b58,_0x33c0a3,{from:_0x51d8d7,quoted:_0xea7099,body:_0x51484f,isCmd:_0x44019e,command:_0x55ae58,args:_0x341016,q:_0x15f027,isGroup:_0x1c8e10,sender:_0x1724af,senderNumber:_0x271809,botNumber2:_0x5daa89,botNumber:_0x1283e8,pushname:_0x37016b,isMe:_0x2ee758,isOwner:_0x1f71a3,groupMetadata:_0x3e01f1,groupName:_0x5009a2,participants:_0x111ba7,groupAdmins:_0x159e15,isBotAdmins:_0x32a29b,isAdmins:_0xc053fe,reply:_0x2a7c48})=>{const _0x3646e0=_0x261a0f;try{let _0x34330f=await fetchJson('https://chatgptforprabath-md.vercel.app/api/gptv1?q='+_0x15f027);return _0x2a7c48(''+_0x34330f[_0x3646e0(0xdb)]);}catch(_0x21f405){console['log'](_0x21f405),_0x2a7c48(''+_0x21f405);}});
