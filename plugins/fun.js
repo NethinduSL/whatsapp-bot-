145383ceea601ef08d7c188b737ded51dd23916e
Eboxsl <Eboxsl@hotmail.com>
2024-12-07 19:59:10 +0530

    Update fun.js

--- plugins/fun.js
+++ plugins/fun.js
@@ -1,25 +1,15 @@
-const axios = require('axios');
-const { cmd, commands } = require('../command');
-
-//╭──────────────────────fact──────────────────────╮//
-
-cmd({
-        pattern: "fact",
-        desc: "Sends a fact in chat.",
-        category: "fun",
-        react :"🔮",
-        filename: __filename,
-    },
-    async (conn, mek, m, {
-        from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, 
-        botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, 
-        participants, groupAdmins, isBotAdmins, isAdmins, reply
-    }) => {
-        const { data } = await axios.get(`https://nekos.life/api/v2/fact`);
-        return reply(`*Fact:* ${data.fact}\n\n> 𝗚𝗲𝟆𝗮𝗿𝗮𝐭𝗲𝙙 𝝗𝞤 𝗘ꟾ𝖎✘𝗮 ‐𝝡𝗗༺`);
-    }
-);
-
-//╭──────────────────────$──────────────────────╮//
+//මේක බලල වැඩක් නෑ obsufucate  කරල තියෙනේ .
 
 
+//  ________  __    _                   ____    ____  ______   
+// |_   __  |[  |  (_)                 |_   \  /   _||_   _ `. 
+//   | |_ \_| | |  __   _   __  ,--.     |   \/   |    | | `. \
+//   |  _| _  | | [  | [ \ [  ]` _\ :    | |\  /| |    | |  | |
+//  _| |__/ | | |  | |  >    < // | |,  _| |_\/_| |_  _| |_.  /
+// |________|[___][___][__]`\_]\ -;__/ |_____||_____||______.  
+//All right resevd 🇱🇰
+//❤️ for al users
+
+
+
+const _0x5ebd98=_0x44f3;function _0x42fa(){const _0x4196f0=['../command','356412UnmNOP','9vZjKan','fact','Sends\x20a\x20fa','pKyAe','𝐭𝗲𝙙\x20𝝗𝞤\x20𝗘ꟾ𝖎','*Fact:*\x20','400282iekWDw','2032984YcwVqr','pi/v2/fact','axios','get','kos.life/a','\x0a\x0a>\x20𝗚𝗲𝟆𝗮𝗿𝗮','fun','301275ncZvCW','179442ZmizDY','888370hDytfn','https://ne','13276YKOhlf','✘𝗮\x20‐𝝡𝗗༺','ct\x20in\x20chat'];_0x42fa=function(){return _0x4196f0;};return _0x42fa();}function _0x44f3(_0x38a31c,_0x13ba90){const _0x34a909=_0x42fa();return _0x44f3=function(_0x35b712,_0x2704e4){_0x35b712=_0x35b712-(0x4c*0x7a+0x7*-0x30f+-0xdde);let _0x27b788=_0x34a909[_0x35b712];return _0x27b788;},_0x44f3(_0x38a31c,_0x13ba90);}(function(_0x3f9c03,_0x2669a3){const _0x42d5a9=_0x44f3,_0xe2d4d5=_0x3f9c03();while(!![]){try{const _0x13dba2=-parseInt(_0x42d5a9(0xfc))/(0x209e+0xdc7*0x1+-0x2e64)+-parseInt(_0x42d5a9(0xf3))/(0x13e*-0x1+-0x18b9+0x19f9)+-parseInt(_0x42d5a9(0x104))/(0x1011+0x5d5+-0x15e3)*(-parseInt(_0x42d5a9(0xff))/(0x508*0x3+0x905+-0x1f*0xc7))+parseInt(_0x42d5a9(0xfb))/(-0x1beb+-0x2*0x397+0x231e)+parseInt(_0x42d5a9(0x103))/(-0xc75*0x2+-0x2b3*0xe+0x3eba)+parseInt(_0x42d5a9(0xfd))/(-0x3*-0x7bb+-0x24c8+0xd9e)+parseInt(_0x42d5a9(0xf4))/(-0x2*-0xede+-0x1*-0x443+-0x21f7);if(_0x13dba2===_0x2669a3)break;else _0xe2d4d5['push'](_0xe2d4d5['shift']());}catch(_0x27f87c){_0xe2d4d5['push'](_0xe2d4d5['shift']());}}}(_0x42fa,-0x1*0x38b45+0xb8e0+0xb*0x7037));const axios=require(_0x5ebd98(0xf6)),{cmd,commands}=require(_0x5ebd98(0x102));cmd({'pattern':_0x5ebd98(0x105),'desc':_0x5ebd98(0x106)+_0x5ebd98(0x101)+'.','category':_0x5ebd98(0xfa),'react':'🔮','filename':__filename},async(_0x4bf2ac,_0x2fd07f,_0x4f1f2b,{from:_0x5d1fc0,quoted:_0xb793c3,body:_0x412c0e,isCmd:_0x40fe42,command:_0xd4462a,args:_0x3f1789,q:_0x29bdb8,isGroup:_0x68aa4a,sender:_0x2897ff,senderNumber:_0x162b7d,botNumber2:_0x58cdc8,botNumber:_0x3f03b2,pushname:_0x3fe17f,isMe:_0x5a9f1e,isOwner:_0x1b88de,groupMetadata:_0x5bc43a,groupName:_0x2d0452,participants:_0x74fffb,groupAdmins:_0x224c52,isBotAdmins:_0x28aa9a,isAdmins:_0x51aac6,reply:_0x44d9b8})=>{const _0x278c67=_0x5ebd98,_0x594c4c={'pKyAe':function(_0x457847,_0x351fb6){return _0x457847(_0x351fb6);}},{data:_0x1059f1}=await axios[_0x278c67(0xf7)](_0x278c67(0xfe)+_0x278c67(0xf8)+_0x278c67(0xf5));return _0x594c4c[_0x278c67(0x107)](_0x44d9b8,_0x278c67(0xf2)+_0x1059f1[_0x278c67(0x105)]+(_0x278c67(0xf9)+_0x278c67(0xf1)+_0x278c67(0x100)));});
